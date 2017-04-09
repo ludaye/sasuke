6487c0c997c1fbf12c55466c97ac761e61b7123d
oresama <353945623@qq.com>
2017-04-10 00:19:51 +0800

    4-10

---
+++ src/sasuke/SasukeContext.java
@@ -0,0 +1,4 @@
+package sasuke;
+
+public class SasukeContext {
+}
--- src/sasuke/util/SasukeUtils.java
+++ src/sasuke/util/SasukeUtils.java
@@ -1,10 +1,12 @@
 package sasuke.util;
 
+import com.google.common.base.Strings;
 import com.intellij.openapi.components.ServiceManager;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+import sasuke.ModuleProperties;
 import sasuke.SasukeSettings;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -14,14 +16,15 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 import java.util.Properties;
 
 public class SasukeUtils {
 
     public static final Properties PROPERTIES = stringToProperties(ServiceManager.getService(
             SasukeSettings.class).getJdbc());
+    public static final String MODULE_DIR = "$MODULE_DIR$";
+    public static final String URL_PREFIX = "file://";
 
     public static Properties stringToProperties(String string) {
         Properties result = new Properties();
@@ -35,20 +38,31 @@ public class SasukeUtils {
         return result;
     }
 
-    public static List<String> getSourceFolder(String moduleFilePath) {
+    public static ModuleProperties getSourceFolder(String moduleFilePath, String modulePath) {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        List<String> strings = new ArrayList<>();
+        ModuleProperties moduleProperties = new ModuleProperties();
         try {
             DocumentBuilder dbd = dbf.newDocumentBuilder();
             Document doc = dbd.parse(new File(moduleFilePath));
             NodeList nodeList = doc.getElementsByTagName("sourceFolder");
             for (int i = 0, len = nodeList.getLength(); i < len; i++) {
                 Element item = (Element) nodeList.item(i);
-                strings.add(item.getAttribute("url"));
+                String isTestSource = item.getAttribute("isTestSource");
+                String type = item.getAttribute("type");
+                Optional<String> url = Optional.ofNullable(item.getAttribute("url")).map(s -> s.replace(MODULE_DIR,
+                        modulePath).replace(URL_PREFIX, ""));
+                if (!Strings.isNullOrEmpty(isTestSource) && "false".equals(isTestSource)) {
+                    item.getAttribute("url");
+                    url.ifPresent(s -> moduleProperties.getSrc().add(s));
+                    break;
+                }
+                if (!Strings.isNullOrEmpty(type) && "java-resource".equals(type)) {
+                    url.ifPresent(s -> moduleProperties.getResources().add(s));
+                }
             }
         } catch (ParserConfigurationException | SAXException | IOException e) {
             throw new RuntimeException("解析文件出错" + e.getMessage(), e);
         }
-        return strings;
+        return moduleProperties;
     }
 }
